aee754421acdc3da7747e38ec62e9ecca860ee55
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-10-11 21:05:13 -0400

    added question struct

---
+++ JeopardyApp/Constants/APIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  APIConstants.swift
+//  JeopardyApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-10-11.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseUrl = ""
+}
+
+enum APIConstants {
+    static let baseUrl = "http://jservice.io/api/random"
+}
---
+++ JeopardyApp/Feature/Question/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  JeopardyApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-10-11.
+//
+
+import Foundation
+
+struct Question: Decodable {
+    let question: String
+    let answer: String
+}
