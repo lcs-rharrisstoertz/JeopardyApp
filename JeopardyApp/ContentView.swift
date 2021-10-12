89b3a559dffbb09eed02ad958b9e1c7aeef04ad4
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-10-11 20:54:52 -0400

    Initial Commit

---
+++ JeopardyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JeopardyApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JeopardyApp/JeopardyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeopardyAppApp.swift
+//  JeopardyApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-10-11.
+//
+
+import SwiftUI
+
+@main
+struct JeopardyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
