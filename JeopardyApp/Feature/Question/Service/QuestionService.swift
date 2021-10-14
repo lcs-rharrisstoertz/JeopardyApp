f44882c6892c1bb2df64a2fe24e6469b225070ea
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-10-14 09:21:16 -0400

    fetchRandomQuestion returned an array, when it should have returned a value of the type Question. Fixed this.

--- JeopardyApp/Feature/Question/Service/QuestionService.swift
+++ JeopardyApp/Feature/Question/Service/QuestionService.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 protocol QuestionService {
-    func fetchRandomQuestion() async throws -> [Question]
+    func fetchRandomQuestion() async throws -> Question
 }
 
 final class QuestionServiceImpl: QuestionService {
-    func fetchRandomQuestion() async throws -> [Question] {
+    func fetchRandomQuestion() async throws -> Question {
         let urlSession = URLSession.shared
         let url = URL(string: APIConstants.baseUrl)
         let (data, _) = try await urlSession.data(from: url!)
-        return try JSONDecoder().decode([Question].self, from: data)
+        return try JSONDecoder().decode(Question.self, from: data)
     }
 }
