720e1667a9c7a61c1bb41bfe87987645f4382b16
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-10-15 01:04:15 -0400

    Kept trying to display API data

--- JeopardyApp/Feature/Question/Model/Question.swift
+++ JeopardyApp/Feature/Question/Model/Question.swift
@@ -8,6 +8,27 @@
 import Foundation
 
 struct Question: Decodable {
-    let question: String
     let answer: String
+    let question: String
 }
+
+//[
+//   {
+//      "id":41746,
+//      "answer":"the New York Yankees",
+//      "question":"(Johnny Gilbert reads.) Drake revealing Emily didn't kill Carson, got her out of jail in time to see this team win the 2000 World Series",
+//      "value":200,
+//      "airdate":"2001-06-22T12:00:00.000Z",
+//      "created_at":"2014-02-11T23:10:55.162Z",
+//      "updated_at":"2014-02-11T23:10:55.162Z",
+//      "category_id":5082,
+//      "game_id":null,
+//      "invalid_count":null,
+//      "category":{
+//         "id":5082,
+//         "title":"jeopardy! place",
+//         "created_at":"2014-02-11T23:10:54.880Z",
+//         "updated_at":"2014-02-11T23:10:54.880Z",
+//         "clues_count":30
+//      }
+//   }
